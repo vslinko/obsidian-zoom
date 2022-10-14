f1dfe6f9cd4199441dd71efeb4d09fec42642667
Dmitry Petrov <dvpetrov.main@gmail.com>
2022-10-14 11:30:57 +0300

    Remove unused icons

--- src/ObsidianZoomPlugin.ts
+++ src/ObsidianZoomPlugin.ts
@@ -1,4 +1,4 @@
-import { Editor, Plugin, addIcon } from "obsidian";
+import { Editor, Plugin } from "obsidian";
 
 import { EditorView } from "@codemirror/view";
 
@@ -12,15 +12,6 @@ import { ZoomFeature } from "./features/ZoomFeature";
 import { ZoomOnClickFeature } from "./features/ZoomOnClickFeature";
 import { LoggerService } from "./services/LoggerService";
 import { SettingsService } from "./services/SettingsService";
-
-addIcon(
-  "obsidian-zoom-zoom-in",
-  `<path fill="currentColor" stroke="currentColor" stroke-width="2" d="M42,6C23.2,6,8,21.2,8,40s15.2,34,34,34c7.4,0,14.3-2.4,19.9-6.4l26.3,26.3l5.6-5.6l-26-26.1c5.1-6,8.2-13.7,8.2-22.1 C76,21.2,60.8,6,42,6z M42,10c16.6,0,30,13.4,30,30S58.6,70,42,70S12,56.6,12,40S25.4,10,42,10z"></path><line x1="24" y1="40" x2="60" y2="40" stroke="currentColor" stroke-width="10"></line><line x1="42" y1="20" x2="42" y2="60" stroke="currentColor" stroke-width="10"></line>`
-);
-addIcon(
-  "obsidian-zoom-zoom-out",
-  `<path fill="currentColor" stroke="currentColor" stroke-width="2" d="M42,6C23.2,6,8,21.2,8,40s15.2,34,34,34c7.4,0,14.3-2.4,19.9-6.4l26.3,26.3l5.6-5.6l-26-26.1c5.1-6,8.2-13.7,8.2-22.1 C76,21.2,60.8,6,42,6z M42,10c16.6,0,30,13.4,30,30S58.6,70,42,70S12,56.6,12,40S25.4,10,42,10z"></path><line x1="24" y1="40" x2="60" y2="40" stroke="currentColor" stroke-width="10"></line>`
-);
 
 export default class ObsidianZoomPlugin extends Plugin {
   protected zoomFeature: ZoomFeature;
